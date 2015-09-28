dd7b7139da5977d7a54792a11523f8576cda4566
Callum Styan <callumstyan@gmail.com>
2015-09-27 22:47:05 -0700

    final version, with comments :beer:

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ import argparse
 import time
 import os
 
-
+# globals
 global maxPort
 global lastPosition
 global fileSize
@@ -16,7 +16,11 @@ asciiMax = 127
 lastPosition = 0
 fileSize = 0
 
-# creates a packet where the arguments char1 and char2 are set as the tcp source port
+# createPacketTwo - takes in two ASCII characters
+# Turns both characters into binary strings, concatenates the strings
+# and turns the result into an integer value. It then creates a TCP packet
+# and sets the source port as the difference between 65535 and the integer.
+# Returns a TCP packet created by scapy.
 def createPacketTwo(char1, char2):
   # get the binary values of both chars without the binary string indicator
   binChar1 = bin(ord(char1))[2:].zfill(8)
@@ -25,21 +29,27 @@ def createPacketTwo(char1, char2):
   # get the integer value of the concatenated binary values
   intPortVal = int(binChar1 + binChar2, 2)
   print "bin value " + str((bin(intPortVal)))
-
+  # craft the packet
   packet = IP(dst=args.destIp)/TCP(dport=80, sport=maxPort - intPortVal)
   return packet
 
-# create a packet when we only have 1 character remaining
+# create a packet when we only have 1 character remaining in the file
+# works exactly the same as createPacketTwo except we only have one character
+# returns a TCP packet created by scapy.
 def createPacketOne(char):
   # get the binary value of the character
   binChar = bin(ord(char))[2:].zfill(8)
   print binChar
   #get the integer value of that binary value
   intPortVal = int(binChar, 2)
-
+  # craft the packet
   packet = IP(dst=args.destIp)/TCP(dport=80, sport=maxPort -intPortVal)
   return packet
 
+# readOneByte - takes in a file descriptor of an open file
+# accesses the global lastPosition variable, and seeks to that byte offset
+# within the file.  Then, read one byte from the file and update the lastPosition.
+# Returns the byte read from the file.
 def readOneByte(fileDescriptor):
   global lastPosition
   fileDescriptor.seek(lastPosition)
@@ -47,22 +57,27 @@ def readOneByte(fileDescriptor):
   lastPosition = fileDescriptor.tell()
   return byte
 
-
+# sendPackets - loops through the file specified as a command line argument.
+# Reads each byte from the file, calls the appropriate packet creation function
+# and sends each packet.  Between each send there is a sleep for a randomized amount
+# of time within a range, also set as a command line argument.
 def sendPackets():
   global fileSize
   global lastPosition
   fileDescriptor = open(args.path, 'r')
   while lastPosition < fileSize:
     if lastPosition == fileSize - 1:
+      # the next byte we read contains the last character in the file
       char = readOneByte(fileDescriptor)
       packet = createPacketOne(char)
     else:
+      # there is at least 2 characters left in the file
       char1 = readOneByte(fileDescriptor)
       char2 = readOneByte(fileDescriptor)
       packet = createPacketTwo(char1, char2)
 
+    # scapy send
     send(packet)
-
     print "sport: " + str(packet.sport)
     time.sleep(randint(1,int(args.sendInterval)))
 
--- server.py
+++ server.py
@@ -4,18 +4,28 @@ logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import IP, TCP, sniff
 import argparse
 
+# globals
 maxPort = 65535
 
-# takes in a packet that passes our sniff filter
+# parsePacket - takes in a packet that passes our sniff filter
+# Gets the difference between 65535 and the source port field in the packet,
+# then gets the binary value of that difference.  If the length of the binary
+# string is greater than 8, then we parse 2 characters from the string, otherwise
+# the string only contains one character.  We convert the binary string to an
+# ASCII character.  You'll notice that we open and close the output file within
+# this function, that's because pythons file library requires the file to be closed
+# for the data from our write calls to actually be written to the file.
 def parsePacket(packet):
   fileDescriptor = open(args.filePath, 'a')
   sport = packet.sport
   difference = maxPort - sport
   binVal = bin(difference)[2:]
-  # print binVal
   binLen = len(binVal)
   if binLen > 8:
+    # binary string contains two ascii characters
+    # the last 8 characters in the string are always the 2nd character
     binChar2 = binVal[-8:]
+    # python trims leading zeroes at the start of our concatenated binary string
     binChar1 = binVal[0:binLen - 8]
     char1 = chr(int(binChar1, 2))
     char2 = chr(int(binChar2, 2))
@@ -23,6 +33,7 @@ def parsePacket(packet):
     fileDescriptor.write(char1)
     fileDescriptor.write(char2)
   else:
+    # binary string contains one ascii character
     char = chr(int(binVal, 2))
     print "Received: " + char
     fileDescriptor.write(char)
@@ -38,5 +49,5 @@ parser.add_argument('-o'
                    , help='Absolute path to where you would like to save packets sent to the server.'
                    , required=True)
 args = parser.parse_args()
-
+# sniff for tcp packets with a destination port of 80, send them to parsePacket
 sniff(filter="tcp and (dst port 80)", prn=parsePacket)
